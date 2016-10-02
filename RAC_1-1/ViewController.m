5de163903e7f6fbd276b65dd5c696cb19f85bf6d
上海-郭朝阳 <guochaoyang@jd.com>
2016-10-02 18:05:22 +0800

    base doc

--- RAC_1-1/ViewController.m
+++ RAC_1-1/ViewController.m
@@ -11,18 +11,156 @@
 
 @interface ViewController ()
 
+@property (nonatomic,strong) NSString * title;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+
+#pragma mark -  aaa
+    
+//    NSArray *array2 = @[@"a",@"b",@"c",];
+//    RACSignal * arrat2Single = [array2.rac_sequence signal];
+//    [arrat2Single subscribeNext:^(id x){
+//        NSLog(@"%@",x);
+//    }];
+    
+//    __block unsigned subscriptions = 0;
+//    RACSignal * loginSignal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber){
+//        subscriptions ++;
+////        [subscriber sendNext:nil];
+//        [subscriber sendCompleted];
+//        return nil;
+//    }];
+//    
+//    [loginSignal subscribeCompleted:^{
+//        NSLog(@"subscription %u", subscriptions);
+//    }];
+//    
+//    [loginSignal subscribeCompleted:^{
+//        NSLog(@"subscription %u", subscriptions);
+//    }];
+    
+    
+//    __block unsigned subscriptions = 0;
+//    RACSignal * loginSignal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber){
+//        subscriptions ++;
+//        //        [subscriber sendNext:nil];
+//        [subscriber sendCompleted];
+//        return nil;
+//    }];
+//    
+//    [loginSignal doCompleted:^{
+//        NSLog(@"about to complete subscription %u", subscriptions);
+//    }];
+//    
+//    [loginSignal subscribeCompleted:^{
+//        NSLog(@"subscription %u", subscriptions);
+//    }];
+//
+    
+//    NSArray * nums = @[@(7),@(9),@(1),@(2),@(3),@(6),@(10)];
+//    RACSequence * numSequence = [nums rac_sequence];
+//    RACSequence * mapSequence = [numSequence map:^id(id x){
+//        return @([(NSNumber *)x intValue] * 10);
+//    }];
+//    
+//    [[mapSequence signal] subscribeNext:^(id x){
+//        NSLog(@"%@",x);
+//    }];
+    
+
+//    NSArray * nums = @[@(7),@(9),@(1),@(2),@(3),@(6),@(10)];
+//    RACSequence * numSequence = [nums rac_sequence];
+//    RACSequence * mapSequence = [numSequence filter:^BOOL(id value){
+//        return ([(NSNumber *)value intValue] > 4);
+//    }];
+//    
+//    [[mapSequence signal] subscribeNext:^(id x){
+//        NSLog(@"%@",x);
+//    }];
+    
+
+    
+    
+//    NSArray * abcs = @[@"a",@"b",@"c"];
+//    NSArray * nums = @[@"1",@"2",@"3"];
+//
+//    RACSequence * concat = [abcs.rac_sequence concat:nums.rac_sequence];
+//    [[concat signal] subscribeNext:^(id x){
+//        NSLog(@"%@",x);
+//    }];
+//    
+    
+    
+    
+    NSArray * abcs = @[@"a",@"b",@"c"];
+    NSArray * nums = @[@"1",@"2",@"3"];
+    
+    RACSequence * seqs = @[abcs,nums].rac_sequence;
+    
+    RACSequence * flatten = [seqs flatten];
+    
+    [[flatten signal] subscribeNext:^(id x){
+        NSLog(@"%@",x);
+    }];
+    
+    
+    
+    
+//    RACSubject * subject = [[RACSubject alloc] init];
+//    [[subject filter:^BOOL(NSString * x){
+//        if ([x isEqualToString:@"123"]) {
+//            return YES;
+//        }
+//        return NO;
+//    }] subscribeNext:^(id x){
+//        NSLog(@"this is true val : %@",x);
+//    }];
+//    
+//    [subject sendNext:@"123" ];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)test1
+{
+    NSArray *array = @[@"a",@"b",@"c"];
+    
+    id aaa = [[array.rac_sequence filter:^BOOL(NSString *str){
+        return YES;
+    }] map:^(NSString *str){
+        NSLog(@"aa: %@",str);
+        return [str stringByAppendingString:@"foobar"];
+    }];
+    
+    @weakify(self);
+    [RACObserve(self, title) subscribeNext:^(NSString * title){
+        @strongify(self);
+        NSLog(@"click me %@",title);
+    }];
+    
+    
+    [[RACObserve(self, title) filter:^BOOL(NSString * title){
+        return [title isEqualToString:@"aaa"];
+    }] subscribeNext:^(NSString * title){
+        NSLog(@"uername is true : %@",title);
+    }];
+    
+    self.title = @"aaa";
+    
+    
+    
+    [[self.loginButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x){
+        NSLog(@"click me 登录 了");
+    }];
+    RAC(self.loginButton,enabled) = [RACSignal combineLatest:@[self.userNameLabel.rac_textSignal] reduce:^(NSString *username){
+        return @(username.length > 0);
+    }];
 }
 
 - (IBAction)loginAction:(id)sender {
--- README.md
+++ README.md
@@ -1,1 +1,134 @@
 # RAC_1-1
+
+
+##### Subscription
+
+订阅者
+热信号
+
+冷信号
+
+```
+ __block unsigned subscriptions = 0;
+    RACSignal * loginSignal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber){
+        subscriptions ++;
+//        [subscriber sendNext:nil];
+        [subscriber sendCompleted];
+        return nil;
+    }];
+    
+    [loginSignal subscribeCompleted:^{
+        NSLog(@"subscription %u", subscriptions);
+    }];
+    
+    [loginSignal subscribeCompleted:^{
+        NSLog(@"subscription %u", subscriptions);
+    }];
+
+
+out put :
+
+2016-10-02 16:58:56.938 RAC_1-1[46719:236962] subscription 1
+2016-10-02 16:58:58.578 RAC_1-1[46719:236962] subscription 2
+```
+
+### Transforming streams
+
+#####  map
+
+```
+    NSArray * nums = @[@(7),@(9),@(1),@(2),@(3),@(6),@(10)];
+    RACSequence * numSequence = [nums rac_sequence];
+    RACSequence * mapSequence = [numSequence map:^id(id x){
+        return @([(NSNumber *)x intValue] * 10);
+    }];
+    
+    [[mapSequence signal] subscribeNext:^(id x){
+        NSLog(@"%@",x);
+    }];
+    
+    输出:
+2016-10-02 17:11:54.827 RAC_1-1[47184:245027] 70
+2016-10-02 17:11:54.829 RAC_1-1[47184:245027] 90
+2016-10-02 17:11:54.830 RAC_1-1[47184:245027] 10
+2016-10-02 17:11:54.831 RAC_1-1[47184:245027] 20
+2016-10-02 17:11:54.831 RAC_1-1[47184:245027] 30
+2016-10-02 17:11:54.832 RAC_1-1[47184:245027] 60
+2016-10-02 17:11:54.833 RAC_1-1[47184:245027] 100
+
+```
+
+
+#### filter
+过滤操作
+
+```
+    NSArray * nums = @[@(7),@(9),@(1),@(2),@(3),@(6),@(10)];
+    RACSequence * numSequence = [nums rac_sequence];
+    RACSequence * mapSequence = [numSequence filter:^BOOL(id value){
+        return ([(NSNumber *)value intValue] > 4);
+    }];
+    
+    [[mapSequence signal] subscribeNext:^(id x){
+        NSLog(@"%@",x);
+    }];
+    输出:
+    2016-10-02 17:14:17.642 RAC_1-1[47265:246460] 7
+2016-10-02 17:14:17.643 RAC_1-1[47265:246460] 9
+2016-10-02 17:14:17.644 RAC_1-1[47265:246460] 6
+2016-10-02 17:14:17.644 RAC_1-1[47265:246460] 10
+    
+```
+
+
+### Combining streams
+
+事物叫合并  信号叫联合
+#### Concatenating  合并
+
+```
+ RACSubject * subject = [[RACSubject alloc] init];
+    [[subject filter:^BOOL(NSString * x){
+        if ([x isEqualToString:@"123"]) {
+            return YES;
+        }
+        return NO;
+    }] subscribeNext:^(id x){
+        NSLog(@"this is true val : %@",x);
+    }];
+    
+    [subject sendNext:@"123" ];
+    输出：
+    2016-10-02 17:27:10.096 RAC_1-1[47411:253513] a
+2016-10-02 17:27:10.097 RAC_1-1[47411:253513] b
+2016-10-02 17:27:10.097 RAC_1-1[47411:253513] c
+2016-10-02 17:27:10.098 RAC_1-1[47411:253513] 1
+2016-10-02 17:27:10.099 RAC_1-1[47411:253513] 2
+2016-10-02 17:27:10.099 RAC_1-1[47411:253513] 3
+```
+
+#### Flattening
+
+
+
+
+
+##### RACSingle
+
+##### RACSubject
+
+
+##### RACSequence
+Sequence 类似于NSArray 
+
+```
+NSArray *array2 = @[@"a",@"b",@"c",];
+    RACSignal * arrat2Single = [array2.rac_sequence signal];
+    [arrat2Single subscribeNext:^(id x){
+        NSLog(@"%@",x);
+    }];
+
+log:a
+	b
+	c
+```
